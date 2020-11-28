6a2299bf37ac86fbd8e9f6617a6741bc860e58dd
Funnisimo <scastette@yahoo.com>
2020-11-28 12:57:19 -0600

    added minify

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,26 @@
 
-// rollup.config.js
+// GW-CANVAS: rollup.config.js
+
+import { terser } from "rollup-plugin-terser";
+
 
 export default [{
   input: 'js/gw.js',
   output: {
-    file: 'dist/gw.js',
+    file: 'dist/gw-canvas-min.js',
     format: 'umd',
     name: 'GW',
     freeze: false,
     extend: true,
+    sourcemap: true,
+    plugins: [terser()]
   }
 },
 {
   // Really?  Do we need this?
   input: 'js/index.js',
   output: {
-    file: 'dist/index.js',
+    file: 'dist/gw-canvas.js',
     format: 'cjs',
     freeze: false,
   }
